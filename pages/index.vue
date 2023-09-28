3060830e94750b15414abee2dfde83023db68317
STUDENT151\STUDENT <vichuta.nice@mail.kmutt.ac.th>
2023-09-28 13:54:53 +0700

    setup project: tailwind ui

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Page:index</div>
+
+  <nuxt-img
+    class="w-auto h-8"
+    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+    alt="Your Company"
+  />
+
+  <BaseTitle>Title Topic component</BaseTitle>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>

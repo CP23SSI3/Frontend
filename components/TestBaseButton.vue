5bf0b7e5f2c44474a00d01f7d7dd38912368879c
STUDENT151\STUDENT <vichuta.nice@mail.kmutt.ac.th>
2023-10-09 20:32:24 +0700

    add: BaseButton

---
+++ components/TestBaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <div>
+      <BaseButton>Default</BaseButton>
+    </div>
+    <BaseButton full>full</BaseButton>
+    <div class="flex gap-2">
+      <BaseButton disabled>disabled</BaseButton>
+      <BaseButton secondary>secondary</BaseButton>
+      <BaseButton outline>outline</BaseButton>
+      <BaseButton negative>negative</BaseButton>
+      <BaseButton :leadingIcon="EnvelopeIcon">leadingIcon</BaseButton>
+      <BaseButton :trailingIcon="UserIcon">trailingIcon</BaseButton>
+      <BaseButton :leadingIcon="TrashIcon" negative>Delete</BaseButton>
+      <BaseButton :trailingIcon="PencilIcon" secondary>Edit</BaseButton>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { PencilIcon, TrashIcon } from '@heroicons/vue/24/outline'
+import { UserIcon, EnvelopeIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/test/component.vue
@@ -0,0 +1,36 @@
+<template>
+  <table class="min-w-full divide-y divide-gray-300">
+    <thead>
+      <tr class="divide-x divide-gray-200">
+        <th
+          scope="col"
+          class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+        >
+          Component Name
+        </th>
+        <th
+          scope="col"
+          class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
+        >
+          Example
+        </th>
+      </tr>
+    </thead>
+    <tbody class="bg-white divide-y divide-gray-200">
+      <tr class="divide-x divide-gray-200">
+        <td
+          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
+        >
+          BaseButton
+        </td>
+        <td class="p-4 text-sm text-gray-500 whitespace-nowrap">
+          <TestBaseButton />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>

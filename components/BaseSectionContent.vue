a6c17b45fde2cc6ac0d6386265fa26fcec324316
STUDENT151\STUDENT <vichuta.nice@mail.kmutt.ac.th>
2023-10-13 18:22:17 +0700

    add: BaseSectionContent

---
+++ components/BaseSectionContent.vue
@@ -0,0 +1,33 @@
+<template>
+  <!--  -->
+  <div class="bg-white rounded-lg shadow" :class="[styleSectionContent]">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  hPadding: {
+    type: Number
+  },
+  vPadding: {
+    type: Number
+  }
+})
+
+const styleSectionContent = computed(() => {
+  let padding = ''
+  if (props.hPadding) {
+    padding = 'px-' + props.hPadding
+  } else {
+    padding = 'px-' + 6
+  }
+
+  if (props.vPadding) {
+    padding = padding + ' py-' + props.vPadding
+  } else {
+    padding = 'py-' + 6
+  }
+  return padding
+})
+</script>

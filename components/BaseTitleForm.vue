631610440efcf2ad40b1421b1472aa81d8894259
STUDENT151\STUDENT <vichuta.nice@mail.kmutt.ac.th>
2023-10-13 18:22:38 +0700

    add: BaseTitleForm

---
+++ components/BaseTitleForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2 class="text-base font-semibold leading-7 text-gray-900">
+      <slot></slot>
+    </h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>

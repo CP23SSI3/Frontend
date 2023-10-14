9953c2cfd1e0f887b4fd78107d4be85d93e05c6b
STUDENT151\STUDENT <vichuta.nice@mail.kmutt.ac.th>
2023-10-14 17:52:32 +0700

    add: BaseTimePicker

--- pages/components.vue
+++ pages/components.vue
@@ -18,75 +18,69 @@
     </thead>
     <tbody class="divide-y divide-gray-200">
       <tr class="divide-x divide-gray-200">
-        <td
-          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
-        >
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           BaseTitle
         </td>
-        <td class="p-4 text-sm whitespace-nowrap">
+        <td class="p-4 text-sm">
           <BaseTitle>Topic 1</BaseTitle>
         </td>
       </tr>
       <tr class="divide-x divide-gray-200">
-        <td
-          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
-        >
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           BaseTitleForm
         </td>
-        <td class="p-4 text-sm whitespace-nowrap">
+        <td class="p-4 text-sm">
           <BaseTitleForm>Topic in Form</BaseTitleForm>
         </td>
       </tr>
       <tr class="divide-x divide-gray-200">
-        <td
-          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
-        >
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           BaseButton
         </td>
-        <td class="p-4 text-s whitespace-nowrap">
+        <td class="p-4 text-s">
           <TestBaseButton />
         </td>
       </tr>
       <tr class="divide-x divide-gray-200">
-        <td
-          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
-        >
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           BaseInput
         </td>
-        <td class="p-4 text-sm text-gray-500 whitespace-nowrap">
+        <td class="p-4 text-sm text-gray-500">
           <TestBaseInput />
         </td>
       </tr>
       <tr class="divide-x divide-gray-200">
-        <td
-          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
-        >
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           BaseSectionContent
         </td>
-        <td class="p-4 text-sm text-gray-500 whitespace-nowrap">
+        <td class="p-4 text-sm text-gray-500">
           <BaseSectionContent :h-padding="6" :v-padding="4">
             Content...
           </BaseSectionContent>
         </td>
       </tr>
       <tr class="divide-x divide-gray-200">
-        <td
-          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
-        >
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           BaseDropdown
         </td>
-        <td class="p-4 text-sm text-gray-500 whitespace-nowrap">
+        <td class="p-4 text-sm text-gray-500">
           <TestBaseDropdown />
         </td>
       </tr>
       <tr class="divide-x divide-gray-200">
-        <td
-          class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0"
-        >
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           BaseDatePicker
         </td>
-        <td class="p-4 text-sm text-gray-500 whitespace-nowrap">
+        <td class="p-4 text-sm text-gray-500">
           <TestBaseDatePicker />
+        </td>
+      </tr>
+      <tr class="divide-x divide-gray-200">
+        <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
+          BaseTimePicker
+        </td>
+        <td class="p-4 text-sm text-gray-500">
+          <TestBaseTimePicker />
         </td>
       </tr>
     </tbody>

0f9abae039904a9cfe80d384818a9ab38fca16b6
MSI\Nice <vichuta.nice@gmail.com>
2023-10-31 18:24:01 +0700

    add: Search and Filter in List Post Page

--- pages/internship/index.vue
+++ pages/internship/index.vue
@@ -1,68 +1,177 @@
 <template>
-  <div>
-    <BaseSectionContent class="px-5 py-5">
-      <div class="flex items-start gap-2 mb-3 md:gap-6">
-        <nuxt-img
-          class="w-12 h-12 rounded-sm shadow-md bg-gray-50"
-          src="/logo-test.png"
-          alt="Your Company"
-        />
-
-        <!-- <div class="flex flex-col w-full gap-2"> -->
-        <div class="flex flex-col-reverse md:flex-col">
-          <div class="text-sm md:text-lg">
-            ประชาสัมพันธ์ บริษัทรับนักศึกษาฝึกงานค่ะ ตามรายละเอียดด้านล่างค่ะ
-          </div>
-
-          <div class="flex gap-2">
-            <div class="text-xs font-semibold leading-6 md:text-sm">
-              แอทโมสฟ์ ดิจิทัล โซลูชั่นส์ almosph.com
-            </div>
-            <BaseBadge color="green" class="hidden sm:flex"
-              >เปิดรับตลอด</BaseBadge
-            >
-          </div>
-        </div>
-
-        <!-- </div> -->
-      </div>
-
-      <div class="flex flex-col gap-2 md:ml-[70px]">
-        <div class="flex flex-wrap gap-2">
-          <span
-            v-for="(item, index) in listPosition"
-            class="inline-flex items-center px-2 py-1 text-xs font-medium text-blue-700 bg-blue-100 rounded-md md:px-3 md:text-sm"
-            >{{ item }}</span
+  <div class="grid items-start grid-cols-4 gap-6">
+    <BaseSectionContent
+      class="hidden col-span-1 gap-2 px-5 py-5 min-h-fit lg:flex lg:flex-col"
+    >
+      <div class="flex flex-col gap-4">
+        <div class="flex items-center justify-between">
+          <BaseTitleForm>Search</BaseTitleForm>
+          <BaseItem
+            class="cursor-pointer hover:text-gray-800"
+            :icon="ArrowPathIcon"
+            >clear</BaseItem
           >
         </div>
-
-        <div
-          class="flex flex-col gap-2 md:items-center md:justify-between md:flex-row"
+        <BaseInput
+          label="Search"
+          id="search"
+          placeholder="name company, post"
+          :iconInput="MagnifyingGlassIcon"
+          v-model="filter.search"
+        ></BaseInput>
+        <BaseDropdown
+          :option-lists="categoryLists"
+          label="หมวดหมู่งาน"
+          v-model="filter.category"
         >
-          <div class="flex flex-col md:flex-row md:flex-wrap md:space-x-6">
-            <BaseItem :icon="BriefcaseIcon">4-6 เดือน</BaseItem>
-            <BaseItem :icon="CurrencyDollarIcon">100-120 บาท/วัน</BaseItem>
-            <BaseItem :icon="MapPinIcon">สีลม เขตบางรัก กรุงเทพมหานคร</BaseItem>
+        </BaseDropdown>
+
+        <div>
+          <BaseLabel>ชื่อตำแหน่งงาน</BaseLabel>
+          <Multiselect
+            v-model="position"
+            mode="tags"
+            :close-on-select="false"
+            :searchable="true"
+            :create-option="true"
+            :options="options"
+            class="multiselect-blue"
+          />
+        </div>
+        <BaseLine />
+        <div class="flex flex-col gap-4">
+          <div class="flex items-center justify-between">
+            <BaseTitleForm>Filter</BaseTitleForm>
+            <BaseItem
+              class="cursor-pointer hover:text-gray-800"
+              :icon="ArrowPathIcon"
+              >clear</BaseItem
+            >
           </div>
 
-          <BaseButton :trailingIcon="Bars3BottomLeftIcon" outline>
-            ดูรายละเอียด
-          </BaseButton>
+          <BaseInput
+            label="ระยะเวลาที่ฝึกงาน"
+            id="workMonth"
+            :icon="BriefcaseIcon"
+            type="number"
+            v-model="filter.workMonth"
+            unitSelect
+          ></BaseInput>
+
+          <BaseInput
+            label="สถานที่ฝึกงาน"
+            id="location"
+            :icon="MapPinIcon"
+            placeholder="ชื่อจังหวัด / แขวง / เขต"
+            v-model="filter.location"
+          ></BaseInput>
         </div>
       </div>
     </BaseSectionContent>
+    <div class="flex flex-col w-full col-span-4 gap-2 lg:col-span-3">
+      <BaseSectionContent class="px-5 py-5" v-for="item in 5">
+        <div class="flex items-start gap-2 mb-3 lg:gap-6">
+          <nuxt-img
+            class="w-12 h-12 rounded-sm shadow-md bg-gray-50"
+            src="/logo-test.png"
+            alt="Your Company"
+          />
+
+          <!-- <div class="flex flex-col w-full gap-2"> -->
+          <div class="flex flex-col-reverse lg:flex-col">
+            <div class="text-sm lg:text-lg">
+              ประชาสัมพันธ์ บริษัทรับนักศึกษาฝึกงานค่ะ ตามรายละเอียดด้านล่างค่ะ
+            </div>
+
+            <div class="flex gap-2">
+              <div class="text-xs font-semibold leading-6 lg:text-sm">
+                แอทโมสฟ์ ดิจิทัล โซลูชั่นส์ almosph.com
+              </div>
+              <BaseBadge color="green" class="hidden lg:flex"
+                >เปิดรับตลอด</BaseBadge
+              >
+            </div>
+          </div>
+
+          <!-- </div> -->
+        </div>
+
+        <div class="flex flex-col gap-2 lg:ml-[70px]">
+          <div class="flex flex-wrap gap-2">
+            <span
+              v-for="(item, index) in listPosition"
+              class="inline-flex items-center px-2 py-1 text-xs font-medium text-blue-700 bg-blue-100 rounded-md lg:px-3 lg:text-sm"
+              >{{ item }}</span
+            >
+          </div>
+
+          <div
+            class="flex flex-col gap-2 md:items-end xl:items-center md:justify-between md:flex-row"
+          >
+            <div class="flex flex-col xl:flex-row xl:space-x-6">
+              <BaseItem :icon="BriefcaseIcon">4-6 เดือน</BaseItem>
+              <BaseItem :icon="CurrencyDollarIcon">100-120 บาท/วัน</BaseItem>
+              <BaseItem :icon="MapPinIcon"
+                >สีลม เขตบางรัก กรุงเทพมหานคร</BaseItem
+              >
+            </div>
+
+            <BaseButton :trailingIcon="Bars3BottomLeftIcon" outline>
+              ดูรายละเอียด
+            </BaseButton>
+          </div>
+        </div>
+      </BaseSectionContent>
+    </div>
   </div>
 </template>
 
 <script setup>
+// definePageMeta({
+//   layout: 'filter'
+// })
+
 import {
   BriefcaseIcon,
   CurrencyDollarIcon,
   MapPinIcon,
-  Bars3BottomLeftIcon
+  Bars3BottomLeftIcon,
+  MagnifyingGlassIcon,
+  ArrowPathIcon
 } from '@heroicons/vue/20/solid'
+
+const filter = ref({
+  search: '',
+  category: { id: 0, text: '--Select--', value: 'default' },
+  position: [],
+  workMonth: null,
+  salary: 0,
+  location: ''
+})
+
+const categoryLists = [
+  { id: 0, text: '--Select--', value: 'default' },
+  { id: 1, text: 'ไอที', value: 'IT' },
+  { id: 2, text: 'งานช่าง', value: 'Engineer' },
+  { id: 3, text: 'บัญชี', value: 'Accountant' }
+]
+
+const options = [
+  'Frontend',
+  'Backend',
+  'UX/UI',
+  'Developer',
+  'Dev',
+  'Full stack'
+]
 
 const listPosition = ['Frontend', 'Backend', 'Developer', 'Infrastructure']
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.multiselect-blue {
+  --ms-tag-bg: #dbeafe;
+  --ms-tag-color: #2563eb;
+  --ms-ring-color: rgba(56, 189, 248, 0.2);
+}
+</style>

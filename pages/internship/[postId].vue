6f61244855c39c6b03fef86b3da60bcc95540c85
MSI\Nice <vichuta.nice@gmail.com>
2023-11-02 17:57:46 +0700

    add: button back in View Detail Page

--- pages/internship/[postId].vue
+++ pages/internship/[postId].vue
@@ -1,8 +1,18 @@
 <template>
   <div>{{ route.params.postId }}</div>
+  <NuxtLink
+    :to="{
+      path: `/internship`
+      // --- เอาไว้ย้อนกลับหน้า page เดิม ---
+      // query: { page: route.query.page }
+    }"
+  >
+    <BaseItem :icon="ChevronLeftIcon" class="cursor-pointer">back</BaseItem>
+  </NuxtLink>
 </template>
 
 <script setup>
+import { ChevronLeftIcon } from '@heroicons/vue/20/solid'
 const route = useRoute()
 </script>
 

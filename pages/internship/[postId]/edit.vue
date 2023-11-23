40df0cb0aae0054ae680aab0cf10936f4a8b0cda
MSI\Nice <vichuta.nice@gmail.com>
2023-11-23 16:11:51 +0700

    add: edit post page

---
+++ pages/internship/[postId]/edit.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Edit
+    {{ post }}
+  </div>
+</template>
+
+<script setup>
+const post = ref([])
+const props = defineProps({
+  post: {
+    type: Object
+  }
+})
+
+post.value = props?.post
+</script>
+
+<style lang="scss" scoped></style>

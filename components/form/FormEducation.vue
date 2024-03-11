25ba473a3a02fcfe49bab09efb6f3b5cae51c7e7
MSI\Nice <vichuta.nice@gmail.com>
2024-03-11 15:10:58 +0700

    add: education mange item

---
+++ components/form/FormEducation.vue
@@ -0,0 +1,140 @@
+<template>
+  <Form
+    @submit="$emit('submit')"
+    v-slot="{ meta, values, errors }"
+    :validation-schema="schemaExperience"
+  >
+    <div
+      class="grid items-start grid-cols-1 px-6 py-4 mt-1 rounded-md gap-x-3 gap-y-4 sm:grid-cols-4 bg-blue-50"
+    >
+      <BaseTitleForm class="text-center col-span-full">
+        {{ editmode ? 'Edit Education' : 'Add Education' }}
+      </BaseTitleForm>
+      <BaseInputField
+        class="sm:col-span-4"
+        label="Degree"
+        id="degree"
+        v-model="education.degree"
+        required
+      ></BaseInputField>
+
+      <BaseInputField
+        class="sm:col-span-2"
+        label="Field"
+        id="field"
+        v-model="education.field"
+      ></BaseInputField>
+      <BaseInputField
+        class="sm:col-span-2"
+        label="School Name"
+        id="schoolName"
+        v-model="education.schoolName"
+        required
+      ></BaseInputField>
+      <Field
+        v-slot="{ field, errors }"
+        name="startedYear"
+        class="sm:col-span-2 lg:col-span-1"
+      >
+        <BaseYearPicker
+          label="Start year"
+          id="startedYear"
+          required
+          v-model="education.startedYear"
+          v-bind="field"
+          placeholder="Select start year"
+          :year-range="[
+            new Date().getFullYear() - 50,
+            new Date().getFullYear()
+          ]"
+        >
+          <template v-slot:error-message v-if="errors"
+            >{{ errors[0] }}
+          </template>
+        </BaseYearPicker>
+      </Field>
+      <Field
+        v-slot="{ field, errors }"
+        name="graduatedYear"
+        class="sm:col-span-1"
+      >
+        <BaseYearPicker
+          label="Graduated Year"
+          id="graduatedYear"
+          v-model="education.graduatedYear"
+          v-bind="field"
+          placeholder="Select graduated year"
+          :disable="!education.startedYear"
+          :year-range="[
+            education.startedYear
+              ? education.startedYear
+              : new Date().getFullYear() - 50,
+            new Date().getFullYear()
+          ]"
+        >
+          <template v-slot:error-message v-if="errors"
+            >{{ errors[0] }}
+          </template>
+        </BaseYearPicker>
+      </Field>
+      <BaseInputField
+        class="sm:col-span-1"
+        label="Grade"
+        id="grade"
+        v-model="education.grade"
+      ></BaseInputField>
+      <BaseTextarea
+        label="Description"
+        id="educationDesc"
+        nametag="description"
+        :rows="10"
+        v-model="education.educationDesc"
+      ></BaseTextarea>
+      <div class="flex flex-row-reverse gap-3 sm:col-span-full">
+        <BaseButton :leadingIcon="CheckIcon" type="submit">
+          {{ editmode ? 'Save' : 'Add' }}
+        </BaseButton>
+        <BaseButton negative @click="$emit('cancel')" type="button">
+          Cancel
+        </BaseButton>
+      </div>
+    </div>
+  </Form>
+</template>
+
+<script setup>
+import { CheckIcon } from '@heroicons/vue/24/solid'
+import { Field, ErrorMessage, Form } from 'vee-validate'
+import * as yup from 'yup'
+
+const props = defineProps({
+  education: {
+    type: Object,
+    required: true
+  },
+  editmode: {
+    type: Boolean,
+    default: false
+  }
+})
+defineEmits(['submit', 'cancel'])
+
+const schemaExperience = yup.object({
+  degree: yup.string().required().max(50),
+  educationDesc: yup.string().max(1500),
+  field: yup.string().nullable().max(100),
+  grade: yup.string().max(5),
+  startedYear: yup.number().required("Please 'select' the working year"),
+  graduatedYear: yup.number().nullable(),
+  schoolName: yup.string().required().max(100)
+})
+</script>
+
+<style lang="scss" scoped>
+.searchselect-blue {
+  --ms-option-bg-selected: #2563eb;
+  --ms-option-bg-selected-pointed: #2563eb;
+  --ms-ring-color: rgba(56, 189, 248, 0.2);
+  --ms-spinner-color: #60a5fa;
+}
+</style>
